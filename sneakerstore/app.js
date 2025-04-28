51a1a2b8a99a123807b9e8c5988e314f9fa6d906
lauxinger <lauxinger09@gmail.com>
2025-04-28 17:31:49 +0800

    Add files via upload

---
+++ sneakerstore/app.js
@@ -0,0 +1,60 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelector("menuItem");
+
+const products =[
+ {
+id:1,
+title:"Air Force",
+price:119
+colors:[
+    {  
+      code:"black",
+      img:"./img/Product/air.png", 
+    },
+    {
+      code:"darkblue",
+      img:"./img/Product/air2.png",
+    },
+   ]
+  }
+  (
+  )
+   id:2,
+   title:"Air Jordan"
+   price:149
+   {
+     code:"lightgray",
+     img:"./img/Product/jordan2.png",
+   }
+   {
+     code:"green",
+     img:"./img/Product/jorden2.png",
+   }
+ ],
+ },
+ {
+    id:3,
+    title: "Blazer",
+    price: 109,
+    colors:[
+     {  
+        code:"green",
+        img:"./img/Product/blazer2.png",
+     }
+     {
+        code:"green",
+        img:"./img/Product/blazer2.png",
+     }
+    ]
+  } 
+  {
+    id:4,
+    title:"Crater",
+    price:129,
+    colors:[ 
+    {
+      code:"black",
+      img:"./img/Product/CharacterData.png",
+    }
+    {
+      code:
